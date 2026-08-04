526bb2a5d1a4addf407d7915c544ca1c6b5bfcac
Mohammad Akib <mhdakib7769@gmail.com>
2026-08-04 09:05:55 +0600

    Database updated
    Updated SQLALCHEMY_DATABASE_URL with the correct connection string for the PostgreSQL database.

--- database.py
+++ database.py
@@ -2,29 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = '1. Connection string
-Copy the connection details for your database.
-Details:
-Shared pooler
-If your database password contains special characters, percent-encode them in the connection string.
-Connection parameters
-host:aws-0-ap-southeast-1.pooler.supabase.com
-port:5432
-database:postgres
-user:postgres.dldkbkgmukmldfvagqjw
-Code:
-File: Code
-```
-postgresql://postgres.dldkbkgmukmldfvagqjw:MhdAkib0812@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres
-```
-
-2. Install Agent Skills (optional)
-Agent Skills give AI coding tools ready-made instructions, scripts, and resources for working with Supabase more accurately and efficiently.
-Code:
-File: Code
-```
-npx skills add supabase/agent-skills
-```'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.dldkbkgmukmldfvagqjw:MhdAkib0812@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
